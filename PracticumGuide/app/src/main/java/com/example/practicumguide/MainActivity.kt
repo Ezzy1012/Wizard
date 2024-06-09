cadfdc5ada918d7540f3e6095b0c55fac44ce702
Ezzy1012 <161321071+Ezzy1012@users.noreply.github.com>
2024-06-09 21:36:55 +0200

    Add files via upload

---
+++ PracticumGuide/app/src/main/java/com/example/practicumguide/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.practicumguide
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val button: Button =findViewById(R.id.buttonNavigate)
+        button.setOnClickListener {
+            button.text = "Go Further"
+            val intent = Intent(this, SecondPage::class.java)
+            SecondPage()
+        }
+    }
+}
